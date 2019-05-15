371c24d7796e32b74d9b34834ae89d56b5f1ea1d
Marcin Koziej <marcin@akcjademokracja.pl>
2019-05-15 22:23:12 +0200

    Kilis bargain

--- syria/init.js
+++ syria/init.js
@@ -1,10 +1,13 @@
 
 
 State.variables.known = {};
-State.variables.annoing = 0;
-State.variables.talkative = 0;
+State.variables.annoing = 0; // scale 0-3?
+State.variables.talkative = 0;  // scale 0-3?
 State.variables.busTime = 0;
 
-UIBar.stow();
+State.variables.lonelyPlanetTopics = ["damascus"];
+State.variables.arabic = [];
+
+Config.ui.stowBarInitially = true;
 
 
